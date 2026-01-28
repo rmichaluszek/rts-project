767d2e8a6f2ae62cf96fdf8cf4ddaf3cb8b1d370
rmichaluszek <rmichaluszek@gmail.com>
2026-01-28 18:44:21 +0100

    Drawing mouse selection

--- main/camera.gd
+++ main/camera.gd
@@ -10,11 +10,12 @@ extends Camera2D
 
 var dragging: bool = false
 var drag_start: Vector2
-
+var selecting: bool = false
+var select_start: Vector2
 
 
 func _input(event):
-	# Start drag when left mouse button is pressed
+
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_MIDDLE:
 			if event.pressed:
@@ -22,6 +23,17 @@ func _input(event):
 				drag_start = get_global_mouse_position()
 			else:
 				dragging = false
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				selecting = true
+				select_start = get_global_mouse_position()
+			else:
+				selecting = false
+				queue_redraw()
+				
+func _draw() -> void:
+	if(selecting):
+		draw_rect(Rect2(select_start-position,get_global_mouse_position()-select_start),Color(0.255, 0.475, 1.0, 0.439))
 
 func _process(delta):
 	
@@ -37,6 +49,10 @@ func _process(delta):
 		self.position += mouse_delta * drag_speed
 	else:
 		self.position += input_vector * speed * delta
+		
+	if selecting:
+		var select_area = get_global_mouse_position() - select_start
+		queue_redraw()
 
 	if self.position.x < camera_safezone.x: self.position.x = camera_safezone.x
 	if self.position.y < camera_safezone.y: self.position.y = camera_safezone.y
