6d0acb89d22a65f5d7876990ba4f59aa63492b4b
rmichaluszek <rmichaluszek@gmail.com>
2026-01-29 21:35:55 +0100

    Basic formation pathfinding

--- main/group_manager.gd
+++ main/group_manager.gd
@@ -12,8 +12,20 @@ func _process(delta: float) -> void:
 
 func move_command(pos):
 	if(current_selected_units!=[]):
-		for u in current_selected_units:
-			u.set_destination(pos)
+		var formation_positions = []
+		var units_distance_to_destination = []
+		for u in range(0,current_selected_units.size()):
+			
+			# reaaaaaly basic and primitive group formation, takes the closest unit to click location and sets its destination the farthers in formation, and the same for every other unit relative to click
+			if(u==0):
+				formation_positions.push_back(Vector2(0,0))
+			else:
+				formation_positions.push_back(Vector2(160*floor((u-1)/6+1),0).rotated(deg_to_rad((u-1)*60)))
+			units_distance_to_destination.push_back(await current_selected_units[u].get_length_to_destination(pos))
+		
+			current_selected_units[u].set_destination(pos+formation_positions[u])
+		
+		
 
 func set_selected_units(array: Array):
 	
--- objects/unit/unit.gd
+++ objects/unit/unit.gd
@@ -19,31 +19,69 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	
+	var new_velocity = Vector2(0,0)
 	if !navigation_agent.is_navigation_finished():
 		var next_agent_position = navigation_agent.get_next_path_position()
 		
 		#change it to smooth speeding up later
-		velocity = global_position.direction_to(next_agent_position) * movement_speed
+		new_velocity = global_position.direction_to(next_agent_position) * movement_speed
+		
+		navigation_agent.set_velocity(new_velocity)
+		
+		move_and_slide()
 	else:
 		# idle if nothing to do
 		set_state(UnitState.IDLE)
-		velocity = Vector2(0,0)
-	move_and_slide()
+		
+		
+	
+
 
 func pathfinfing_setup():
 	await get_tree().physics_frame
 
+func get_length_to_destination(pos: Vector2):
+	if pos != Vector2(0,0):
+		navigation_agent.target_position = pos
+		await navigation_agent.path_changed
+		var length = get_path_length()
+		navigation_agent.target_position = position
+		return(length)
+			
 func set_destination(pos: Vector2):
 	if pos != Vector2(0,0):
 		navigation_agent.target_position = pos
+		$TargetPositionMark.position = pos
+		await navigation_agent.path_changed
 		set_state(UnitState.MOVE)
+		
+		print("i am ",get_name(), " and my path length is: ", str(get_path_length()))
+
 
 func set_state(state: UnitState):
 	$StateDebug.text = "STATE: " + str(UnitState.keys()[state])
 	currentState = state
+	
+func get_path_length():
+	var path = navigation_agent.get_current_navigation_path()
+	if path.is_empty():
+		return 0.0
+
+	var length := 0.0
+	for i in range(path.size() - 1):
+		length += path[i].distance_to(path[i + 1])
+
+	return length
 
 # for now this is outline
 func set_selected(selected: bool):
 	isSelected = selected
-	if(isSelected): $Selected.visible = true
-	else: $Selected.visible = false
+	if(isSelected): 
+		$Selected.visible = true
+		$TargetPositionMark.visible = true
+	else: 
+		$Selected.visible = false
+		$TargetPositionMark.visible = false
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
